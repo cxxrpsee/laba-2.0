2a541ce2c274f7e391cd5d86dc97cf5ddf9c4426
Артём <brilank056@gmail.com>
2025-09-26 08:45:26 +0300

    main

---
+++ laba 2.0/laba 2.0.cpp
@@ -0,0 +1,23 @@
+﻿// Lab_2.1.cpp
+// < Лебединський Артем >
+// Лабораторна робота № 2.
+// Лінійні програми.
+// Варіант 11
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+	double m; // вхідний параметр
+	double z1; // результат обчислення 1-го виразу
+	double z2; // результат обчислення 2-го виразу
+	cout << "m = "; cin >> m;
+	z1 = (1 - 2 * (sin(m)) * (sin(m))) / (1 + sin(2 * m));
+	z2 = (1 - tan(m)) / (1 + tan(m));
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	cin.get();
+	return 0;
+}
